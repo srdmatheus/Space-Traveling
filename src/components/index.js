5c78061af6f49d06ed16ed54f2d6d5575528f191
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-05-24 01:33:08 -0300

    import all items from a single file

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Food } from "./Food"
+export { default as Header } from "./Header"
+export { default as Input } from "./Input"
+export { default as Modal } from "./Modal"
+export { default as ModalAddFood } from "./ModalAddFood"
+export { default as ModalEditFood } from "./ModalEditFood"
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,10 +1,8 @@
 import { Component } from 'react';
 
-import Header from '../../components/Header';
 import api from '../../services/api';
-import Food from '../../components/Food';
-import ModalAddFood from '../../components/ModalAddFood';
-import ModalEditFood from '../../components/ModalEditFood';
+import { Header, ModalAddFood, ModalEditFood, Food, } from "../../components"
+
 import { FoodsContainer } from './styles';
 
 class Dashboard extends Component {
